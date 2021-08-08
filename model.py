420ce5e0ff8e37664c1e3fee679cf912b160a27c
RamadanAhmed <ramadan.ahmedr93@gmail.com>
2021-08-08 15:20:00 +0200

    Initial Commit

---
+++ model.py
@@ -0,0 +1,55 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        if 'center' not in line:
+            lines.append(line)
+images = []
+mesaurements = []
+for line in lines:
+    for i in range(3):
+        source_path = line[i]
+        filename = "data/" + source_path.lstrip()
+        image = cv2.imread(filename)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        images.append(image)
+    
+    mesaurement = float(line[3])
+    mesaurment_left = mesaurement + 0.2
+    mesaurment_right = mesaurement - 0.2
+    mesaurements.append(mesaurement)
+    mesaurements.append(mesaurment_left)
+    mesaurements.append(mesaurment_right)
+
+
+x_train = np.array(images)
+y_train = np.array(mesaurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D, Cropping2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20),(0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
